8a3a47fa5a7b2ae34a6365796c8456242acf7885
GabRodrigues23 <gabrigues23@gmail.com>
2025-12-03 21:05:27 -0300

    :truck: change repository injector from product_repository to api_product_repository

--- lib/modules/products/product_module.dart
+++ lib/modules/products/product_module.dart
@@ -1,15 +1,15 @@
-import 'package:djsync/modules/products/data/repository/product_repository.dart';
-import 'package:djsync/modules/products/data/repository/product_repository_interface.dart';
-import 'package:djsync/modules/products/view/product_page.dart';
-import 'package:djsync/modules/products/viewmodel/products_viewmodel.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:djsync/modules/products/data/repository/api_product_repository.dart';
+import 'package:djsync/modules/products/data/repository/product_repository_interface.dart';
+import 'package:djsync/modules/products/viewmodel/products_viewmodel.dart';
+import 'package:djsync/modules/products/view/product_page.dart';
 
 class ProductModule extends Module {
   @override
   void binds(Injector i) {
     super.binds(i);
 
-    i.add<ProductRepositoryInterface>(ProductRepository.new);
+    i.add<ProductRepositoryInterface>(ApiProductRepository.new);
     i.addSingleton(ProductsViewmodel.new);
   }
 
